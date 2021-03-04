35c941e79f523d82d635235e6b8cc770994372b0
Jorge Parga <parga@adobe.com>
2021-03-04 11:28:51 -0500

    fixed header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,56 @@
-import React from 'react';
-import { defineMessages, useIntl } from 'react-intl';
+import React from "react";
+import { defineMessages, useIntl } from "react-intl";
 
 const messages = defineMessages({
-    language: {
-        id: 'Header.language',
-        defaultMessage: 'Language:'
-    },
-    spanish: {
-        id: 'Header.spanish',
-        defaultMessage: 'Spanish'
-    },
-    english: {
-        id: 'Header.english',
-        defaultMessage: 'English'
-    }
-})
+  language: {
+    id: "Header.language",
+    defaultMessage: "Language:",
+  },
+  spanish: {
+    id: "Header.spanish",
+    defaultMessage: "Spanish",
+  },
+  english: {
+    id: "Header.english",
+    defaultMessage: "English",
+  },
+});
 
 interface HeaderProps {
-  setLocale: React.Dispatch<React.SetStateAction<string>>
+  setLocale: React.Dispatch<React.SetStateAction<string>>;
   locale: string;
 }
 
-export function Header ({setLocale, locale}: HeaderProps) {
-    const {formatMessage} = useIntl();
-    const handleOnChange = (event) => {
-        setLocale(event.target.value);
-    }
-    return (
-        <header className="App-header">
-        <label htmlFor="cars">{formatMessage(messages.language)}</label>
-        <select className="p-select" id="cars" onChange={handleOnChange} value={locale}>
-            <option value="es">{formatMessage(messages.spanish)}</option>
-            <option value="en">{formatMessage(messages.english)}</option>
+export function Header({ setLocale, locale }: HeaderProps) {
+  const { formatMessage } = useIntl();
+  const handleOnChange = (event) => {
+    setLocale(event.target.value);
+  };
+  return (
+    <header className="App-header">
+      <div className="logo">
+        <img
+          src="https://feather-insurance.com/_next/static/media/logo.0d69221106425e5288907e514db23d99.svg"
+          aria-label="Home"
+        />
+        <div className="name">
+          <h1>Pluma</h1>
+        </div>
+      </div>
+      <div className="language-selector-box">
+        <label htmlFor="language-selector">
+          {formatMessage(messages.language)}
+        </label>
+        <select
+          className="p-select"
+          id="language-selector"
+          onChange={handleOnChange}
+          value={locale}
+        >
+          <option value="es">{formatMessage(messages.spanish)}</option>
+          <option value="en">{formatMessage(messages.english)}</option>
         </select>
-        </header>
-    );
+      </div>
+    </header>
+  );
 }
-
